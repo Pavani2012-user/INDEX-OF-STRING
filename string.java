8832c370e8cc106a734655f6ef326db867e0eb6e
Pavani2012-user <pavanibattu006@gmail.com>
2025-01-25 15:59:20 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string {
+   public static void main(String[] args) {
+            String inputString = "Hello, World!";
+            int startIndex = 7;
+            int endIndex = 12;
+            String substring = inputString.substring(startIndex, endIndex);
+            System.out.println("Extracted substring: " + substring);
+   }
+    }
+    
+
